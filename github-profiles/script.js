dd754cdb6e4da17cc35f9c32021a52229e99dccb
Nikhéel Pachaangé <nikhil1pachange@gmail.com>
2026-08-28 10:03:05 +0530

    Refactor script.js for improved readability

--- github-profiles/script.js
+++ github-profiles/script.js
@@ -1,5 +1,4 @@
 const APIURL = 'https://api.github.com/users/'
-
 const main = document.getElementById('main')
 const form = document.getElementById('form')
 const search = document.getElementById('search')
